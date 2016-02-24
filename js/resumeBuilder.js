979ed13a8a7c212d0c26e5da69fc6c46053d414e
Ben Stein <bumstain@gmail.com>
2016-02-24 23:57:43 +0000

    Loaded bio details as an object to append/prepend to DOM

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,32 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name": "Ben Stein",
+	"role": "Web Developer",
+	"contacts": {
+		"email": "bpstein@mail.com",
+		"github": "bumstain",
+		"twitter": "@bumstain",
+		"location": "London, United Kingdom"
+	},
+	"picture": "images/fry.jpg",
+	"welcomeMessage": "I'm currently working on freelance web development projects.",
+	"skills": ["JavaScript", "Ruby", "Ionic", "Front-end Development"]
+};
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+var formattedContactInfo = [];
+formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
